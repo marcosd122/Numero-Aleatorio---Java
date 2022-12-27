6a6977d99d6fff4d07a73f3424ab52d0c85ff68e
marcosd122 <marcos.adv.guimaraes@gmail.com>
2022-12-27 11:45:30 -0400

    commit inicial

---
+++ README.md
@@ -0,0 +1,17 @@
+# Atividade -- Numero Aleatório
+
+Projeto de desenvolvimento Numero Aleatório - Java
+
+Atividade curso Java
+
+## Tarefas
+
+O controle das tarefas desse projeto será realizado no Github
+
+## Ícones
+
+- :package: nova funcionalidade
+- :up: atualização
+- :bug: correção de bug
+- :checkered_flag: release
+
---
+++ src/numero/aleatório/NumeroAleatório.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numero.aleatório;
+
+import java.util.*;
+
+/**
+ *
+ * @author Marcos
+ */
+public class NumeroAleatório {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int count;
+        
+        Random rnum = new Random();
+        //Gerar numero entre 0 e 500
+        
+        System.out.println("Numeros aleatorios :");
+        for (count = 0; count<=5;count++) {
+            System.out.println(rnum.nextInt(500)); // Gerar numero aleatório
+        }
+    }
+    
+}
